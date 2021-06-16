06d48c360b4c71ecf2748bd16799463e296344c8
HJLEE <silverlight2017@gmail.com>
2021-06-16 15:37:12 +0900

    TTEst가 안올라가는 이유는??

--- src/main/java/com/wooti/tech/activity/controll/TTEst.java
+++ src/main/java/com/wooti/tech/activity/controll/TTEst.java
@@ -1,11 +1,14 @@
 package com.wooti.tech.activity.controll;
 
+import android.util.Log;
+
 public class TTEst {
 
     public String s;
 
     public TTEst(String s){
         this.s = s;
+        Log.e("HJLEE", "ssds");
     }
 
 }
